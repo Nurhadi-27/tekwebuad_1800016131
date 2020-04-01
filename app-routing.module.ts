f91a848a3be0449e9107976e0e05acd321329c3a
Nurhadi-27 <62319906+Nurhadi-27@users.noreply.github.com>
2020-04-01 22:42:35 +0700

    Add files via upload

--- app-routing.module.ts
+++ app-routing.module.ts
@@ -1,8 +1,48 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { Praktikum3Component } from './pages/praktikum3/Praktikum3.Component';
+import { Praktikum4Component } from './pages/praktikum4/Praktikum4.Component';
+import { Praktikum5Component } from './pages/praktikum5/Praktikum5.Component';
+import { Praktikum6Component } from './pages/praktikum6/Praktikum6.Component';
+import { Praktikum7Component } from './pages/praktikum7/Praktikum7.Component';
+import { Praktikum8Component } from './pages/praktikum8/Praktikum8.Component';
+import { Praktikum9Component } from './pages/praktikum9/Praktikum9.Component';
+import { Praktikum10Component } from './pages/praktikum10/Praktikum10.Component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+      {
+       path: 'praktikum3',
+       component:Praktikum3Component
+      },
+      {
+       path: 'praktikum4',
+       component:Praktikum4Component
+      },
+      {
+       path: 'praktikum5',
+       component:Praktikum5Component
+      },
+      {
+       path: 'praktikum6',
+       component:Praktikum6Component
+      },
+      {
+       path: 'praktikum7',
+       component:Praktikum7Component
+      },
+      {
+       path: 'praktikum8',
+       component:Praktikum8Component
+      },
+      {
+       path: 'praktikum9',
+       component:Praktikum9Component
+      },
+      {
+       path: 'praktikum10',
+       component:Praktikum10Component
+      }
+];      
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- app.module.ts
+++ app.module.ts
@@ -3,10 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Praktikum3Component } from './pages/praktikum3/Praktikum3.component';
+import { Praktikum4Component } from './pages/praktikum4/Praktikum4.component';
+import { Praktikum5Component } from './pages/praktikum5/Praktikum5.component';
+import { Praktikum6Component } from './pages/praktikum6/Praktikum6.component';
+import { Praktikum7Component } from './pages/praktikum7/Praktikum7.component';
+import { Praktikum8Component } from './pages/praktikum8/Praktikum8.component';
+import { Praktikum9Component } from './pages/praktikum9/Praktikum9.component';
+import { Praktikum10Component } from './pages/praktikum10/Praktikum10.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Praktikum3Component,
+    Praktikum4Component,
+    Praktikum5Component,
+    Praktikum6Component,
+    Praktikum7Component,
+    Praktikum8Component,
+    Praktikum9Component,
+    Praktikum10Component
   ],
   imports: [
     BrowserModule,
